1357f807a33ffd8e6854e1440b234373370ee1b8
Eduardo <eduardosantossm@hotmail.com>
2026-04-13 19:21:29 -0300

    feat: add `PATCH` `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,6 +2,7 @@ import database from "infra/database.js";
 import email from "infra/email.js";
 import webserver from "infra/webserver";
 import { NotFoundError } from "infra/errors.js";
+import user from "models/user.js";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutos
 
@@ -33,7 +34,7 @@ async function findOneValidById(tokenId) {
         action: "Faça um novo cadastro",
       });
     }
-    
+
     return result.rows[0];
   }
 }
@@ -59,6 +60,34 @@ async function create(userId) {
   }
 }
 
+async function markTokenAsUsed(activationTokenId) {
+  const usedActivationToken = await runUpdateQuery(activationTokenId);
+  return usedActivationToken;
+
+  async function runUpdateQuery(activationTokenId) {
+    const result = await database.query({
+      text: `
+      UPDATE 
+        user_activation_tokens
+      SET
+        used_at = timezone('utc',now()),
+        updated_at = timezone('utc', now())
+      WHERE
+        id = $1
+      RETURNING 
+        *
+    ; `,
+      values: [activationTokenId],
+    });
+    return result.rows[0];
+  }
+}
+
+async function activateUserByUserId(userId) {
+  const activatedUser = await user.setFeatures(userId, ["create:session"]);
+  return activatedUser;
+}
+
 async function sendEmailToUser(user, activationToken) {
   await email.send({
     from: "Eduardo <contato@example.com>",
@@ -77,6 +106,8 @@ const activation = {
   sendEmailToUser,
   create,
   findOneValidById,
+  markTokenAsUsed,
+  activateUserByUserId,
 };
 
 export default activation;
